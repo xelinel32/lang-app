c1ae2e37b85774be514874701e6dd13c9685cb30
xelinel32 <artemsedlyar99@gmail.com>
2020-07-01 22:40:19 +0300

    change header component

--- src/components/v-header.vue
+++ src/components/v-header.vue
@@ -6,23 +6,56 @@
       v-model="drawer"
       class="hidden-md-and-up"
     >
-      <v-list dense>
-        <v-list-item-group color="primary">
-          <v-list-item
-            v-for="item in menuItems"
-            :key="item.title"
-            :to="item.route"
-            exact-active-class="active"
-          >
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
+      <v-list>
+        <v-list-item to="/books" exact-active-class="active">
+          <v-list-item-icon>
+            <v-icon left>mdi-book</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title> Read</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/learn" exact-active-class="active">
+          <v-list-item-icon>
+            <v-icon left>mdi-book</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title> Learn word</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="'/profile/' + id" exact-active-class="active">
+          <v-list-item-icon>
+            <v-icon left>mdi-face-profile</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title> Profile</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item block to="/login" exact-active-class="active">
+          <v-list-item-icon>
+            <v-icon left>mdi-login</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Log in</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/register" exact-active-class="active">
+          <v-list-item-icon>
+            <v-icon left>mdi-cellphone-lock</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Register</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block to="/logout?message=logout"
+            >Logout
+            <v-icon> <v-icon left>mdi-login-variant</v-icon></v-icon>
+          </v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
     <v-main>
       <v-toolbar dark color="primary">
@@ -33,16 +66,44 @@
         <v-toolbar-title v-text="'Lang App'"></v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn to="/books" text class="primary" exact-active-class="active">
+            <v-icon left>mdi-book</v-icon>
+            Read
+          </v-btn>
+          <v-btn to="/learn" text class="primary" exact-active-class="active">
+            <v-icon left>mdi-book</v-icon>
+            Learn word
+          </v-btn>
           <v-btn
-            v-for="item in menuItems"
-            :key="item.title"
-            :to="item.route"
+            :to="'/profile/' + id"
             text
             class="primary"
             exact-active-class="active"
           >
-            <v-icon left>{{ item.icon }}</v-icon>
-            {{ item.title }}
+            <v-icon left>mdi-face-profile</v-icon>
+            Profile
+          </v-btn>
+          <v-btn
+            to="/logout?message=logout"
+            text
+            class="primary"
+            exact-active-class="active"
+          >
+            <v-icon left>mdi-login-variant</v-icon>
+            Logout
+          </v-btn>
+          <v-btn to="/login" text class="primary" exact-active-class="active">
+            <v-icon left>mdi-login</v-icon>
+            Log in
+          </v-btn>
+          <v-btn
+            to="/register"
+            text
+            class="primary"
+            exact-active-class="active"
+          >
+            <v-icon left>mdi-cellphone-lock</v-icon>
+            Register
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
@@ -54,40 +115,10 @@
 export default {
   name: 'v-header',
   data: () => ({
-    menuItems: [
-      {
-        title: 'Read',
-        route: '/books',
-        icon: 'mdi-book',
-      },
-      {
-        title: 'Learn word',
-        route: '/learn',
-        icon: 'mdi-file-word-box',
-      },
-      {
-        title: 'Profile',
-        route: '/profile',
-        icon: 'mdi-face-profile',
-      },
-      {
-        title: 'Logout',
-        route: '/logout',
-        icon: 'mdi-login-variant',
-      },
-      {
-        title: 'Log in',
-        route: '/login',
-        icon: 'mdi-login',
-      },
-      {
-        title: 'Register',
-        route: '/register',
-        icon: 'mdi-cellphone-lock',
-      },
-    ],
     drawer: false,
+    id: '123',
   }),
+  computed: {},
 };
 </script>
 
